20c38572de9b781355ac968f47a62a3ae199da8a
André <andre.jose_@hotmail.com>
2025-11-05 03:44:05 -0300

    Finalizando

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080";
+const API_URL = "barbersho-juan.us-east-2.elasticbeanstalk.com";
 
 // ==================== LOGIN ====================
 async function loginUsuario() {
